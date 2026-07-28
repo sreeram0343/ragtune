b16177cf9de2c63ebda4f14f8279ea27db278bc8
sreeram0343 <sreerammurali2005@gmail.com>
2026-07-28 20:46:27 +0530

    feat(cache): add package exports for intelligent cache module

--- cache/__init__.py
+++ cache/__init__.py
@@ -1,3 +1,13 @@
-from .redis_client import EnterpriseCacheManager
+from .core.provider import BaseCacheProvider, InMemoryLRUCacheProvider, RedisCacheProvider
+from .core.keys import TenantCacheKeyBuilder
+from .engines.semantic_cache import SemanticCacheEngine
+from .engines.single_flight import SingleFlightLock
+from .engines.invalidation import CacheInvalidationEngine
+from .telemetry.metrics import CacheTelemetryTracker
+from .manager import IntelligentCacheManager
 
-__all__ = ["EnterpriseCacheManager"]
+__all__ = [
+    "BaseCacheProvider", "InMemoryLRUCacheProvider", "RedisCacheProvider",
+    "TenantCacheKeyBuilder", "SemanticCacheEngine", "SingleFlightLock",
+    "CacheInvalidationEngine", "CacheTelemetryTracker", "IntelligentCacheManager"
+]
