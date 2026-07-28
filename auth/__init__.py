c58384dc4d13eb347e6ba73d5bde291eb5e83028
sreeram0343 <sreerammurali2005@gmail.com>
2026-07-28 21:22:55 +0530

    fix(auth): update auth package init to export all subpackages cleanly

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,8 +1,10 @@
+from . import domain, security, storage, services, api
 from .domain.models import SecurityContext, UserDomain, OrganizationDomain, WorkspaceDomain
 from .domain.permissions import OrgRole, WorkspaceRole, Permission
 from .api.routes import router as auth_router
 
 __all__ = [
+    "domain", "security", "storage", "services", "api",
     "SecurityContext", "UserDomain", "OrganizationDomain", "WorkspaceDomain",
     "OrgRole", "WorkspaceRole", "Permission", "auth_router"
 ]
