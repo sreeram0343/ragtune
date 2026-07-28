57c6a4d6b28790cdf9e5beb78236662a52497d9b
sreeram0343 <sreerammurali2005@gmail.com>
2026-07-28 21:34:46 +0530

    feat(text2sql): add package exports for Enterprise Text-to-SQL module

--- text2sql/__init__.py
+++ text2sql/__init__.py
@@ -1,3 +1,13 @@
-from .engine import Text2SQLEngine, SQLGenerationResult
+from .domain import ColumnSchema, TableSchema, SQLValidationResult, SQLExecutionMetrics, StructuredSQLResult
+from .schema import SchemaIntrospector
+from .generator import SQLGenerator
+from .validator import SQLValidator
+from .execution import SQLExecutionEngine
+from .interpreter import ResultInterpreter
+from .engine import EnterpriseText2SQLEngine
 
-__all__ = ["Text2SQLEngine", "SQLGenerationResult"]
+__all__ = [
+    "ColumnSchema", "TableSchema", "SQLValidationResult", "SQLExecutionMetrics", "StructuredSQLResult",
+    "SchemaIntrospector", "SQLGenerator", "SQLValidator", "SQLExecutionEngine",
+    "ResultInterpreter", "EnterpriseText2SQLEngine"
+]
